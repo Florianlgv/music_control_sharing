14718f224e5bc6ae20c34b9f67b6e4c439c712d1
Florianlgv <florianlangevine37@gmail.com>
2023-11-01 11:04:52 +0100

    feat: setup serializer

--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def main(request):
-    return(HttpResponse("<h1>Hello</h1>"))
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
+
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
