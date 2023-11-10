91db5beedf85876a397aca0b0ed54cbac9cefde0
Florianlgv <florianlangevine37@gmail.com>
2023-11-10 18:30:41 +0100

    delete api request for playback

--- spotify/urls.py
+++ spotify/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path("redirect", spotify_callback),
     path("is-authenticated", IsAuthenticated.as_view()),
     path("current-song", CurrentSong.as_view()),
-    path("pause", PauseSong.as_view()),
-    path("play", PlaySong.as_view()),
     path("get-token", GetToken.as_view()),
 ]
