3fcb8f87f8665a603efc82474149628ae5f61f6e
Florianlgv <florianlangevine37@gmail.com>
2023-11-10 18:26:45 +0100

    MUI4 -> MUI5

--- frontend/src/components/JoinRoomPage.js
+++ frontend/src/components/JoinRoomPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { TextField, Button, Grid, Typography } from "@mui/material";
 
 const JoinRoomPage = () => {
   const [roomCode, setRoomCode] = useState("");
