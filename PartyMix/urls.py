f215114008dee97a1296be1543e2acec698c730f
Florianlgv <florianlangevine37@gmail.com>
2023-11-21 10:11:15 +0100

    rename into Partymix

--- PartyMix/urls.py
+++ PartyMix/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for music_controller project.
+URL configuration for PartyMix project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
