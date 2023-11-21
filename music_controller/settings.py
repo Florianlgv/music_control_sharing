6ed88da2c2a5836c6d123b440d5e95bb15425094
Florianlgv <florianlangevine37@gmail.com>
2023-11-21 09:38:54 +0100

    .

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.63", "192.168.10.124", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.1.63", "192.168.10.110", "localhost", "127.0.0.1"]
 
 
 # Application definition
